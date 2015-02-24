58477f36e020489bb29efd7751e3b221a2522b0d
Davide Dell'Erba <delda@virgilio.it>
2015-02-25 00:18:45 +0100

    Drawing texts and bullets together

--- src/main.c
+++ src/main.c
@@ -4,21 +4,26 @@
 static Window *s_window;             // main window view
 static TextLayer *s_layerHours;      // text layer for hours
 static TextLayer *s_layerMinutes;    // test layer for minutes
-static Layer *s_dotsHours;        // bullets for hours
-static Layer *s_dotsMinutes;      // bullets for minutes
+static Layer *s_dotsHours;           // bullets for hours
+//static Layer *s_dotsMinutes;         // bullets for minutes
+char *s_textBase[]={"1","2","4","8","16","32"};
+static TextLayer *temp[6];
 
-static void update_proc_dotsHours(Layer *layer, GContext *gContext){
-  graphics_context_set_fill_color(gContext, GColorBlack);
-  graphics_fill_circle(gContext, GPoint(20,8), 8);
-  graphics_context_set_fill_color(gContext, GColorBlack);
-  graphics_fill_circle(gContext, GPoint(53,8), 8);
-  graphics_context_set_fill_color(gContext, GColorBlack);
-  graphics_fill_circle(gContext, GPoint(86,8), 8);
-  graphics_context_set_fill_color(gContext, GColorBlack);
-  graphics_fill_circle(gContext, GPoint(120,8), 8);
+static void fillRow(Layer *layer, GContext *gContext, GPoint startPosition, int width, int height, int bulletsNumber){
+  for(int i=0; i<bulletsNumber; i++){
+    graphics_context_set_fill_color(gContext,GColorBlack);
+    graphics_fill_circle(gContext, GPoint((20*(i+1)),24), 8);
+    temp[i] = text_layer_create(GRect((20*(i+1)-8), 0, 16, 18));
+    text_layer_set_background_color(temp[i], GColorClear);
+    text_layer_set_font(temp[i], fonts_get_system_font(FONT_KEY_GOTHIC_14));
+    text_layer_set_text_color(temp[i], GColorBlack);
+    text_layer_set_text_alignment(temp[i], GTextAlignmentCenter);
+    text_layer_set_text(temp[i], s_textBase[i]);
+    layer_add_child(layer, text_layer_get_layer(temp[i]));
+  }
 }
 
-static void update_proc_dotsMinutes(Layer *layer, GContext *gContext){
+/*static void update_proc_dotsMinutes(Layer *layer, GContext *gContext){
   graphics_context_set_fill_color(gContext, GColorBlack);
   graphics_fill_circle(gContext, GPoint(20,8), 8);
   graphics_context_set_fill_color(gContext, GColorBlack);
@@ -31,37 +36,35 @@ static void update_proc_dotsMinutes(Layer *layer, GContext *gContext){
   graphics_fill_circle(gContext, GPoint(100,8), 8);
   graphics_context_set_fill_color(gContext, GColorBlack);
   graphics_fill_circle(gContext, GPoint(120,8), 8);
+}*/
+
+static void update_hours(Layer *layer, GContext *gContext){
+  fillRow(layer, gContext, GPoint(20, 20), 24, 120, 6);
 }
 
 static void window_load(Window *window){
-  // Create time layer for hours and minutes
-  s_layerHours = text_layer_create(GRect(0, 0, 144, 40));
-  text_layer_set_background_color(s_layerHours, GColorClear);
-  text_layer_set_text_color(s_layerHours, GColorBlack);
-  text_layer_set_text(s_layerHours, "8 4 2 1");
+//  s_layerMinutes = text_layer_create(GRect(0, 61, 144, 40));
+//  text_layer_set_background_color(s_layerMinutes, GColorClear);
+//  text_layer_set_text_color(s_layerMinutes, GColorBlack);
+//  text_layer_set_text(s_layerMinutes, "32   16   8   4   2   1");
   
-  s_layerMinutes = text_layer_create(GRect(0, 61, 144, 40));
-  text_layer_set_background_color(s_layerMinutes, GColorClear);
-  text_layer_set_text_color(s_layerMinutes, GColorBlack);
-  text_layer_set_text(s_layerMinutes, "32 16 8 4 2 1");
-  
-  s_dotsHours = layer_create(GRect(0, 41, 144, 40));
+  s_dotsHours = layer_create(GRect(0, 21, 144, 40));
   layer_add_child(window_get_root_layer(window), s_dotsHours);
-  layer_set_update_proc(s_dotsHours, update_proc_dotsHours);
+  layer_set_update_proc(s_dotsHours, update_hours);
 
-  s_dotsMinutes = layer_create(GRect(0, 81, 144, 40));
-  layer_add_child(window_get_root_layer(window), s_dotsMinutes);
-  layer_set_update_proc(s_dotsMinutes, update_proc_dotsMinutes);
+//  s_dotsMinutes = layer_create(GRect(0, 81, 144, 40));
+//  layer_add_child(window_get_root_layer(window), s_dotsMinutes);
+//  layer_set_update_proc(s_dotsMinutes, update_proc_dotsMinutes);
 
   // Improve the style
-  text_layer_set_font(s_layerHours, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  text_layer_set_text_alignment(s_layerHours, GTextAlignmentCenter);
-  text_layer_set_font(s_layerMinutes, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  text_layer_set_text_alignment(s_layerMinutes, GTextAlignmentCenter);
+//  text_layer_set_font(s_layerHours, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+//  text_layer_set_text_alignment(s_layerHours, GTextAlignmentCenter);
+//  text_layer_set_font(s_layerMinutes, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+//  text_layer_set_text_alignment(s_layerMinutes, GTextAlignmentCenter);
   
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_layerHours));
-  layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_layerMinutes));
+//  layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_layerHours));
+//  layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_layerMinutes));
 }
 
 static void window_unload(){
