a5cfe1fd994feb8d58206b8bfa7ce310fa654dff
Davide Dell'Erba <delda@virgilio.it>
2015-02-27 10:31:54 +0100

    Responsive creation of bullets and numbers

--- src/main.c
+++ src/main.c
@@ -5,15 +5,26 @@ static Window *s_window;             // main window view
 static TextLayer *s_layerHours;      // text layer for hours
 static TextLayer *s_layerMinutes;    // test layer for minutes
 static Layer *s_dotsHours;           // bullets for hours
-//static Layer *s_dotsMinutes;         // bullets for minutes
+static Layer *s_dotsMinutes;         // bullets for minutes
 char *s_textBase[]={"1","2","4","8","16","32"};
 static TextLayer *temp[6];
 
 static void fillRow(Layer *layer, GContext *gContext, GPoint startPosition, int width, int height, int bulletsNumber){
+  int currentWidth;
+  int widthSingleLayer = (int) width/(bulletsNumber-1);
+  printf("%d / %d = %d", width , bulletsNumber, widthSingleLayer);
   for(int i=0; i<bulletsNumber; i++){
+    if(i==0){
+      currentWidth = startPosition.x;
+    }else if(i==(bulletsNumber-1)){
+      currentWidth = startPosition.x + width;
+    }else{
+      currentWidth = startPosition.x + widthSingleLayer * i;
+    }
     graphics_context_set_fill_color(gContext,GColorBlack);
-    graphics_fill_circle(gContext, GPoint((20*(i+1)),24), 8);
-    temp[i] = text_layer_create(GRect((20*(i+1)-8), 0, 16, 18));
+    printf("width: %d", currentWidth);
+    graphics_fill_circle(gContext, GPoint(currentWidth,24), 8);
+    temp[i] = text_layer_create(GRect((currentWidth-8), 0, 16, 18));
     text_layer_set_background_color(temp[i], GColorClear);
     text_layer_set_font(temp[i], fonts_get_system_font(FONT_KEY_GOTHIC_14));
     text_layer_set_text_color(temp[i], GColorBlack);
@@ -23,48 +34,25 @@ static void fillRow(Layer *layer, GContext *gContext, GPoint startPosition, int 
   }
 }
 
-/*static void update_proc_dotsMinutes(Layer *layer, GContext *gContext){
-  graphics_context_set_fill_color(gContext, GColorBlack);
-  graphics_fill_circle(gContext, GPoint(20,8), 8);
-  graphics_context_set_fill_color(gContext, GColorBlack);
-  graphics_fill_circle(gContext, GPoint(40,8), 8);
-  graphics_context_set_fill_color(gContext, GColorBlack);
-  graphics_fill_circle(gContext, GPoint(60,8), 8);
-  graphics_context_set_fill_color(gContext, GColorBlack);
-  graphics_fill_circle(gContext, GPoint(80,8), 8);
-  graphics_context_set_fill_color(gContext, GColorBlack);
-  graphics_fill_circle(gContext, GPoint(100,8), 8);
-  graphics_context_set_fill_color(gContext, GColorBlack);
-  graphics_fill_circle(gContext, GPoint(120,8), 8);
-}*/
-
 static void update_hours(Layer *layer, GContext *gContext){
-  fillRow(layer, gContext, GPoint(20, 20), 24, 120, 6);
+  fillRow(layer, gContext, GPoint(20, 20), 110, 24, 5);
+}
+
+static void update_minutes(Layer *layer, GContext *gContext){
+  fillRow(layer, gContext, GPoint(20, 20), 110, 24, 6);
 }
 
 static void window_load(Window *window){
-//  s_layerMinutes = text_layer_create(GRect(0, 61, 144, 40));
-//  text_layer_set_background_color(s_layerMinutes, GColorClear);
-//  text_layer_set_text_color(s_layerMinutes, GColorBlack);
-//  text_layer_set_text(s_layerMinutes, "32   16   8   4   2   1");
+  Layer *window_layer = window_get_root_layer(window);
+  GRect windowBounds = layer_get_bounds(window_layer);
   
-  s_dotsHours = layer_create(GRect(0, 21, 144, 40));
+  s_dotsHours = layer_create(GRect(0, 21, windowBounds.size.w, 40));
   layer_add_child(window_get_root_layer(window), s_dotsHours);
   layer_set_update_proc(s_dotsHours, update_hours);
 
-//  s_dotsMinutes = layer_create(GRect(0, 81, 144, 40));
-//  layer_add_child(window_get_root_layer(window), s_dotsMinutes);
-//  layer_set_update_proc(s_dotsMinutes, update_proc_dotsMinutes);
-
-  // Improve the style
-//  text_layer_set_font(s_layerHours, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-//  text_layer_set_text_alignment(s_layerHours, GTextAlignmentCenter);
-//  text_layer_set_font(s_layerMinutes, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-//  text_layer_set_text_alignment(s_layerMinutes, GTextAlignmentCenter);
-  
-  // Add it as a child layer to the Window's root layer
-//  layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_layerHours));
-//  layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_layerMinutes));
+  s_dotsMinutes = layer_create(GRect(0, 61, windowBounds.size.w, 40));
+  layer_add_child(window_get_root_layer(window), s_dotsMinutes);
+  layer_set_update_proc(s_dotsMinutes, update_minutes);
 }
 
 static void window_unload(){
